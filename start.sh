f6403aa78f339a8c3731eef63afe2e9cc90f8b4e
Alexsahaa <36375661+AlX33a@users.noreply.github.com>
2024-04-30 10:01:47 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,8 +6,4 @@ sudo apt install curl software-properties-common ca-certificates apt-transport-h
 wget -O- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose -y
-
-
-minikube start --listen-address=0.0.0.0
-minikube kubectl --config view
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
